6a442fcc342c38bb4e9a03d2f7371a64c6b7128e
Jaideep Singh <jay61226@gmail.com>
2025-02-05 00:49:24 +0530

    default  movies  render changes

--- src/components/MovieSearch/_hooks/useMovieSearch.js
+++ src/components/MovieSearch/_hooks/useMovieSearch.js
@@ -1,21 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { searchMovies } from "../../../redux/thunk/searchMovies";
 
-
 export function useMovieSearch() {
   const [query, setQuery] = useState("");
+  const [defaultQuery] = useState("spider");
+  const [isDefault, setIsDefault] = useState(true);
   const dispatch = useDispatch();
   const { currentPage, totalPages, loading } = useSelector((state) => state.movies);
 
+  useEffect(() => {
+
+    dispatch(searchMovies({ query: defaultQuery, page: 1 }));
+  }, [dispatch, defaultQuery]);
+
   const handleSearch = (page = 1) => {
     if (query.trim()) {
+      setIsDefault(false);
       dispatch(searchMovies({ query, page }));
     }
   };
 
   const handlePageChange = (page) => {
-    handleSearch(page);
+    if (isDefault) {
+      dispatch(searchMovies({ query: defaultQuery, page }));
+    } else {
+      handleSearch(page);
+    }
   };
 
   return {
